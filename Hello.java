fb1ec840c95391d886f011a0cb803da71f824bef
wathworship <wathworship@gmail.com>
2014-10-13 20:59:30 +0700

    hello

---
+++ Hello.java
@@ -0,0 +1,15 @@
+package SRT;
+
+import java.util.Scanner;
+
+public class Hello {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int number = s.nextInt();
+		String input = s.next();
+		
+		for (int i = 0; i < number ; i++) {
+			System.out.println(input);
+		}
+	}
+}
