07b285e0d07cafcc2e838eec22c31922c83797fc
mysorrow <n.mysorrow@gmail.com>
2014-10-16 21:38:53 +0700

    เพิ่มchackboxเมนูอาหาร

--- Shop.php
+++ Shop.php
@@ -2,13 +2,14 @@
 ?>
 <html>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>ก๋วยเตี๋ยวต้มยำบ้ำนลำยจุดข้ำงวัง</title>
+<title>ก๋วยเตี๋ยวต้มยำบ้ำนลำยจุดข้างวัง</title>
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-<div>
+<div class="content">
 
-<center><h1>ก๋วยเตี๋ยวต้มยำบ้านลายจุดข้งวัง</h1></center>
+<center><h1>ก๋วยเตี๋ยวต้มยำบ้านลายจุดข้างวัง</h1></center>
 <h3>ก๋วยเตี๋ยวต้มยำ บ้านลายจุดข้างวัง .... เลือกท็อปปิ้งได้ตามใจ
 จุดเด่นอยู่ที่น้ำซุปรสเข้ม เพิ่มการปรุงต้มยำในแบบเราเข้าไป
 น้ำมะนำวแท้ๆ พริกเผาเข้มข้น ถั่วบดทำเอง 
@@ -19,16 +20,40 @@
 บาท หรือจะเพิ่มกุ้งหรือปลาหมึกได้
 อีกนิดนึง...แต่ละชามเราทาใหม่ ปรุงใหม่ ลวกใหม่ สำหรับชามต่อชามเท่นั้น อาจจะต้องมีรอบ้าง
 เพราะลูกค้าสามารถรีเควสได้ว่าจะทานเผ็ดหรือไม่เผ็ด หรือน้ำใสก็มีสำหรับเด็กน้อยค่ะ</h3>
-<h2>ท็อปปิ้ง</h2>
+<h2><b>ท็อปปิ้ง</b></h2>
+<form method="post" action="">
 <div class="checkbox"">
   <label>
     <input type="checkbox" value="">กุ้งแม่น้ำ		1 ตัวละ 55 บาท<input type="text"size ="2"value="">ตัว</br>
     <input type="checkbox" value="">ปลาหมึก		1 ตัวละ 55 บาท<input type="text"size ="2"value="">ตัว</br>
     </label>
 </div>
-<h2>ก๋วยเตี๋ยว</h2>
-<h3>เส้นก๋วยเตี๋ยว</h3>
-
+<h2><b>ก๋วยเตี๋ยว</b></h2>
+<h4>เส้นก๋วยเตี๋ยว</h4>
+<div class="checkbox"">
+ <label>
+	<input type="checkbox" value="">เส้นเล็ก&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<input type="checkbox" value="">เส้นหมี่</br>
+	<input type="checkbox" value="">บะหมี่&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<input type="checkbox" value="">วุ้นเส้น</br>
+	</label>
 </div>
+<h4>เพิ่มเครื่อง</h4>
+<div class="checkbox"">
+ <label>
+     <input type="checkbox" value=""> ใส่ผัก &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     <input type="checkbox" value=""> ไม่ใส่ผัก </br>
+     <input type="checkbox" value=""> น้ำตก&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     <input type="checkbox" value=""> น้ำใส </br>
+	<input type="checkbox" value=""> ใส่ไข่มะตูน &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<input type="checkbox" value=""> กุ้ง</br>
+	<input type="checkbox" value=""> ปลาเมึก
+	
+	</label>
+</div>
+<button type="submit" class="btn btn-default">บันทึกรายการ</button>
+<button type="reset" class="btn btn-default">ยกเลิกรายการ</button>
+</div>
+</form>
 </body>
 </html>
